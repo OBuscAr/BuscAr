5582907a2aec1954022ca204c5c47ad137a326f5
Diego Hurtado de Mendoza <34421725+DiegoHDMGZ@users.noreply.github.com>
2025-11-13 11:32:11 -0300

    Add id and unique constraint to `LineStop` (#16)

--- backend/app/models/line_stop.py
+++ backend/app/models/line_stop.py
@@ -1,10 +1,15 @@
+from uuid import uuid4
+
 from app.core.database import Base
-from sqlalchemy import Column, ForeignKey, Integer
+from sqlalchemy import UUID, Column, ForeignKey, Integer, UniqueConstraint
 
 
 class LineStop(Base):
     __tablename__ = "line_stop"
 
-    line_id = Column(ForeignKey("line.id"), index=True, primary_key=True)
-    stop_id = Column(ForeignKey("stop.id"), index=True, primary_key=True)
-    stop_order = Column(Integer, index=True, primary_key=True, nullable=False)
+    id = Column(UUID, primary_key=True, index=True, default=uuid4)
+    line_id = Column(ForeignKey("line.id"), index=True)
+    stop_id = Column(ForeignKey("stop.id"), index=True)
+    stop_order = Column(Integer, index=True, nullable=False)
+
+    __table_args__ = (UniqueConstraint("line_id", "stop_id", "stop_order"),)
