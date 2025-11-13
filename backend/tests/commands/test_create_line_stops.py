be3bc6a22967799618679fb131bee35bacd67b33
DiegoHDMGZ <dhurtadodemendoza@gmail.com>
2025-11-12 22:28:48 -0300

    add test for the line stops command

---
+++ backend/tests/commands/test_create_line_stops.py
@@ -0,0 +1,37 @@
+from app.commands.create_line_stops import create_line_stops
+from app.core.database import SessionLocal
+from app.models import LineDirection, LineModel, LineStopModel, StopModel
+
+
+def test_create_line_stops():
+    """
+    GIVEN  an existing line and an existing stop in database
+    WHEN   the `create_line_stops` is called
+    THEN   the related line stops should be created
+    """
+    # GIVEN
+    session = SessionLocal()
+    line = LineModel(id=2607, name="8084-10", direction=LineDirection.MAIN)
+    stop = StopModel(
+        id=1211391,
+        name="Terminal Metrô Butantã",
+        address="",
+        latitude=0,
+        longitude=0,
+    )
+
+    session.add(line)
+    session.add(stop)
+    session.commit()
+
+    # WHEN
+    create_line_stops()
+
+    # THEN
+    line_stops = session.query(LineStopModel).order_by("stop_order").all()
+    assert len(line_stops) == 2
+    first, second = line_stops
+    assert first.stop_id == second.stop_id == stop.id
+    assert first.line_id == second.line_id == line.id
+    assert first.stop_order == 1
+    assert second.stop_order == 20
