f4863f69001fdc8e8722fe114ee6447284cc5c5f
DiegoHDMGZ <dhurtadodemendoza@gmail.com>
2025-11-15 14:36:08 -0300

    add `calculate_carbon_emission` endpoint

---
+++ backend/tests/repositories/myclimate_client/test_calculate_carbon_emission.py
@@ -0,0 +1,31 @@
+import pytest
+from app.repositories.myclimate_client import calculate_carbon_emission
+from app.schemas import VehicleType
+from tests.factories.schemas import CarbonEmissionFactory
+from tests.helpers import MyclimateHelper
+
+
+@pytest.mark.parametrize("vehicle_type", list(VehicleType))
+def test_response(vehicle_type: VehicleType):
+    """
+    GIVEN  a carbon emission response to be returned by the Myclimate API
+    WHEN   the `calculate_carbon_emission` is called
+    THEN   the related response should be returned
+    """
+    # GIVEN
+    expected_emission = CarbonEmissionFactory.build()
+    distance = 5
+    endpoint_mock = MyclimateHelper.mock_carbon_emission(
+        response=expected_emission,
+        distance=distance,
+        vehicle_type=vehicle_type,
+    )
+
+    # WHEN
+    returned_emission = calculate_carbon_emission(
+        distance=distance, vehicle_type=vehicle_type
+    )
+
+    # THEN
+    assert returned_emission == expected_emission.emission
+    assert endpoint_mock.call_count == 1
