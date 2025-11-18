132966574853cb17c20af19386c58c95c995464d
DiegoHDMGZ <dhurtadodemendoza@gmail.com>
2025-11-18 20:56:29 -0300

    add `DailyLineEmission` table

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,4 +1,5 @@
 # flake8: noqa: F401
+from app.models.daily_line_emission import DailyLineEmission as DailyLineEmissionModel
 from app.models.line import Line as LineModel
 from app.models.line import LineDirection
 from app.models.line_stop import LineStop as LineStopModel
