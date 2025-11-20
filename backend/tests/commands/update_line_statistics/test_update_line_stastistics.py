65ab93ce48164b13faf9fa610f897bf4bffe702d
DiegoHDMGZ <dhurtadodemendoza@gmail.com>
2025-11-20 12:15:51 -0300

    add test to create daily line statistic with different date

--- backend/tests/commands/update_line_statistics/test_update_line_stastistics.py
+++ backend/tests/commands/update_line_statistics/test_update_line_stastistics.py
@@ -1,5 +1,5 @@
 import math
-from datetime import datetime
+from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 
 from app.commands.update_daily_line_statistics import (
@@ -60,6 +60,47 @@ def test_create_daily_line_statistics_different_line(mocker: MockerFixture):
         session.query(DailyLineStatisticsModel).filter_by(line_id=target_line.id).one()
     )
     assert db_daily_line_statistic.date == today
+    assert math.isclose(
+        db_daily_line_statistic.distance_traveled, expected_distance, abs_tol=1e-3
+    )
+
+
+def test_create_daily_line_statistics_different_date(mocker: MockerFixture):
+    """
+    GIVEN  a daily line statistics in database and new SPTrans data for
+           a different date
+    WHEN   the `update_daily_line_statistics` is called
+    THEN   a daily line statistics should be created for the new date
+    """
+    # GIVEN
+    session = SessionLocal()
+    other_date = datetime.now(tz=SAO_PAULO).date() - timedelta(days=3)
+    today = datetime.now(tz=SAO_PAULO).date()
+    target_line = LineFactory.create_sync()
+    DailyLineStatisticsFactory.create_sync(line_id=target_line.id, date=other_date)
+
+    expected_distance = 3
+    mock = mocker.patch(
+        UPDATE_VEHICLE_POSITIONS_NAME,
+        return_value={target_line.id: expected_distance},
+    )
+
+    SPTransHelper.mock_get_vehicles_positions(
+        response=SPTransLinesVehiclesResponseFactory.build()
+    )
+
+    # WHEN
+    update_daily_line_statistics()
+
+    # THEN
+    mock.assert_called()
+    session = SessionLocal()
+    assert session.query(DailyLineStatisticsModel).count() == 2
+
+    db_daily_line_statistic = (
+        session.query(DailyLineStatisticsModel).filter_by(date=today).one()
+    )
+    assert db_daily_line_statistic.line_id == target_line.id
     assert math.isclose(
         db_daily_line_statistic.distance_traveled, expected_distance, abs_tol=1e-3
     )
