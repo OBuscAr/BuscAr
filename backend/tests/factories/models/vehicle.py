9b32b3c04967f564cec3a53a60dc649f42181ff9
DiegoHDMGZ <dhurtadodemendoza@gmail.com>
2025-11-19 23:35:01 -0300

    improve latitude and longitude values in factory

--- backend/tests/factories/models/vehicle.py
+++ backend/tests/factories/models/vehicle.py
@@ -1,6 +1,10 @@
+import random
+
 from app.models import VehicleModel
+from polyfactory import Use
 from polyfactory.factories.sqlalchemy_factory import SQLAlchemyFactory
 
 
 class VehicleFactory(SQLAlchemyFactory[VehicleModel]):
-    pass
+    latitude = Use(lambda: random.uniform(-90, 90))
+    longitude = Use(lambda: random.uniform(-90, 90))
