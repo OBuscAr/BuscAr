e7eb27d07525b314a58672a9a734cc14a6e5e3fd
DiegoHDMGZ <dhurtadodemendoza@gmail.com>
2025-11-20 11:05:47 -0300

    add `DailyLineStatisticsFactory`

--- backend/tests/factories/models/__init__.py
+++ backend/tests/factories/models/__init__.py
@@ -1,3 +1,4 @@
 # flake8: noqa: F401
+from tests.factories.models.daily_line_statistics import DailyLineStatisticsFactory
 from tests.factories.models.line import LineFactory
 from tests.factories.models.vehicle import VehicleFactory
