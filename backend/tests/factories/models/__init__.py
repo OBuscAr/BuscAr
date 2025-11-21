d1cc92cdde4ee85942c2669fc2464f38bddfac31
DiegoHDMGZ <dhurtadodemendoza@gmail.com>
2025-11-21 19:07:55 -0300

    add factory

--- backend/tests/factories/models/__init__.py
+++ backend/tests/factories/models/__init__.py
@@ -2,4 +2,5 @@
 from tests.factories.models.daily_line_statistics import DailyLineStatisticsFactory
 from tests.factories.models.line import LineFactory
 from tests.factories.models.line_stop import LineStopFactory
+from tests.factories.models.user_route import UserRouteFactory
 from tests.factories.models.vehicle import VehicleFactory
