c2cc4f77fed55c826f504b28389812472823e1e8
DiegoHDMGZ <dhurtadodemendoza@gmail.com>
2025-11-21 20:43:27 -0300

    refactor security envs

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -15,6 +15,11 @@ class Settings(BaseSettings):
     # Database
     DATABASE_URL: str = ""
 
+    # Security
+    SECRET_KEY: str = ""
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
