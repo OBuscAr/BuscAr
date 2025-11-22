8c29164c674fb01e56ba061df3d7747d156ac7cb
DiegoHDMGZ <dhurtadodemendoza@gmail.com>
2025-11-21 21:33:49 -0300

    rename function

--- backend/app/repositories/user_repository.py
+++ backend/app/repositories/user_repository.py
@@ -1,8 +1,16 @@
+from typing import Optional
+
 from sqlalchemy.orm import Session
+
 from app.models.user import User
 
-def get_by_email(db: Session, email: str):
+
+def get_user_by_email(db: Session, email: str) -> Optional[User]:
+    """
+    Return the user of the given `email`.
+    """
     return db.query(User).filter(User.email == email).first()
+
 
 def create_user(db: Session, user: User) -> User:
     """Adiciona um novo usuário ao banco de dados."""
--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -2,7 +2,7 @@ from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
 
 from app.core.security import check_password, create_token
-from app.repositories.user_repository import get_by_email
+from app.repositories.user_repository import get_user_by_email
 from app.schemas import LoginResponse
 
 
@@ -10,7 +10,7 @@ def authenticate_user(db: Session, email: str, senha: str) -> LoginResponse:
     """
     Authenticate user.
     """
-    user = get_by_email(db, email)
+    user = get_user_by_email(db, email)
     if not user or not check_password(senha, user.senha_hash):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
