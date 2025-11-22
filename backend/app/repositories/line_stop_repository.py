5a6ac4a76befd84fa197653dca4aab4d47aa1359
Diego Hurtado de Mendoza <34421725+DiegoHDMGZ@users.noreply.github.com>
2025-11-22 15:52:02 -0300

    Fix line stop finding when calculating emission (#54)

--- backend/app/repositories/line_stop_repository.py
+++ backend/app/repositories/line_stop_repository.py
@@ -1,8 +1,8 @@
+from typing import Optional
+
 from sqlalchemy import select
-from sqlalchemy.exc import NoResultFound
 from sqlalchemy.orm import Session
 
-from app.exceptions import NotFoundError
 from app.models import StopModel
 from app.models.line_stop import LineStop
 
@@ -20,16 +20,16 @@ class LineStopRepository:
         return db.execute(query).scalars().all()
 
     @staticmethod
-    def get_line_stop(db: Session, line_id: int, stop_id: int) -> LineStop:
+    def get_first_line_stop(
+        db: Session, line_id: int, stop_id: int
+    ) -> Optional[LineStop]:
         """
-        Get the line_stop for the given line and stop.
-        If the line stop does not exist, an exception will be raised.
+        Get the line_stop for the given line and stop. In case of multiple
+        options, return the one with lowest `stop_order`.
         """
-        try:
-            return (
-                db.query(LineStop)
-                .filter(LineStop.line_id == line_id, LineStop.stop_id == stop_id)
-                .one()
-            )
-        except NoResultFound:
-            raise NotFoundError(f"A parada {stop_id} não pertence à linha {line_id}")
+        return (
+            db.query(LineStop)
+            .filter(LineStop.line_id == line_id, LineStop.stop_id == stop_id)
+            .order_by(LineStop.stop_order)
+            .first()
+        )
