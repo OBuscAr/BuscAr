6d25bd2e7ffba3b858604d22c34e5f568cdc183b
DiegoHDMGZ <dhurtadodemendoza@gmail.com>
2025-11-23 20:21:03 -0300

    add retries

--- backend/app/repositories/myclimate_client.py
+++ backend/app/repositories/myclimate_client.py
@@ -1,5 +1,13 @@
+import logging
+
 import requests
 from requests.auth import HTTPBasicAuth
+from tenacity import (
+    before_sleep_log,
+    retry,
+    stop_after_attempt,
+    wait_random_exponential,
+)
 
 from app.core.config import settings
 from app.exceptions import MyclimateError
@@ -9,7 +17,15 @@ BUS_FUEL_CONSUMPTION = 46.2
 CARBON_EMISSION_URL = f"{settings.MYCLIMATE_PREFIX_URL}/v1/car_calculators.json"
 AUTH = HTTPBasicAuth(settings.MYCLIMATE_USERNAME, settings.MYCLIMATE_PASSWORD)
 
+logger = logging.getLogger(__name__)
 
+
+@retry(
+    reraise=True,
+    before_sleep=before_sleep_log(logger, logging.INFO),
+    stop=stop_after_attempt(max_attempt_number=3),
+    wait=wait_random_exponential(multiplier=1, min=2, max=6),
+)
 def calculate_carbon_emission(distance: float, vehicle_type: VehicleType) -> float:
     """
     Calculate the carbon emission (in kg) for a given distance.
--- backend/app/repositories/sptrans_client.py
+++ backend/app/repositories/sptrans_client.py
@@ -1,13 +1,28 @@
+import logging
+
 import requests
 from pydantic import TypeAdapter
 from requests.cookies import RequestsCookieJar
+from tenacity import (
+    before_sleep_log,
+    retry,
+    stop_after_attempt,
+    wait_random_exponential,
+)
 
 from app.core.config import settings
 from app.schemas import SPTransLine, SPTransLinesVehiclesResponse
 
+logger = logging.getLogger(__name__)
 LOGIN_URL = f"{settings.SPTRANS_PREFIX_URL}/Login/Autenticar"
 
 
+@retry(
+    reraise=True,
+    before_sleep=before_sleep_log(logger, logging.INFO),
+    stop=stop_after_attempt(max_attempt_number=3),
+    wait=wait_random_exponential(multiplier=1, min=2, max=6),
+)
 def login() -> RequestsCookieJar:
     """
     Login to Olho Vivo API and return the cookies to use in other endpoint calls.
@@ -21,6 +36,12 @@ LINES_URL = f"{settings.SPTRANS_PREFIX_URL}/Linha"
 LINES_LOOK_UP_URL = f"{LINES_URL}/Buscar"
 
 
+@retry(
+    reraise=True,
+    before_sleep=before_sleep_log(logger, logging.INFO),
+    stop=stop_after_attempt(max_attempt_number=3),
+    wait=wait_random_exponential(multiplier=1, min=2, max=6),
+)
 def get_lines(credentials: RequestsCookieJar, pattern: str) -> list[SPTransLine]:
     """
     Get all the lines that contains the given `pattern`.
@@ -40,6 +61,12 @@ def get_lines(credentials: RequestsCookieJar, pattern: str) -> list[SPTransLine]
 POSITION_URL = f"{settings.SPTRANS_PREFIX_URL}/Posicao"
 
 
+@retry(
+    reraise=True,
+    before_sleep=before_sleep_log(logger, logging.INFO),
+    stop=stop_after_attempt(max_attempt_number=3),
+    wait=wait_random_exponential(multiplier=1, min=2, max=6),
+)
 def get_live_vehicles_positions(
     credentials: RequestsCookieJar,
 ) -> SPTransLinesVehiclesResponse:
