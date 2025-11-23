95aab78ff4c26e76f78ec46d7c1d3dd5d0103595
DiegoHDMGZ <dhurtadodemendoza@gmail.com>
2025-11-23 18:03:01 -0300

    add `get_user_route` repository function

--- backend/app/repositories/user_route_repository.py
+++ backend/app/repositories/user_route_repository.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Query, Session
 
 from app.models import UserRouteModel
 
@@ -11,3 +11,14 @@ def create_user_route(db: Session, user_route: UserRouteModel) -> UserRouteModel
     db.commit()
     db.refresh(user_route)
     return user_route
+
+
+def get_user_routes(db: Session, user_id: int) -> Query[UserRouteModel]:
+    """
+    Get the routes of the given user, ordered by decreasing created time.
+    """
+    return (
+        db.query(UserRouteModel)
+        .filter_by(user_id=user_id)
+        .order_by(UserRouteModel.created_at.desc())
+    )
