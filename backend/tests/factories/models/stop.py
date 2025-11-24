120dbc764d4522b3ea5a67ac1f28800bb2825f4d
DiegoHDMGZ <dhurtadodemendoza@gmail.com>
2025-11-24 12:20:47 -0300

    improve stop factory

--- backend/tests/factories/models/stop.py
+++ backend/tests/factories/models/stop.py
@@ -1,4 +1,5 @@
 from app.models import StopModel
+from polyfactory import Use
 from polyfactory.factories.sqlalchemy_factory import SQLAlchemyFactory
 
 
@@ -6,3 +7,6 @@ class StopFactory(SQLAlchemyFactory[StopModel]):
     __check_model__ = False
     __set_relationships__ = True
     __set_as_default_factory_for_type__ = True
+
+    latitude = Use(lambda: StopFactory.__random__.uniform(-90, 90))
+    longitude = Use(lambda: StopFactory.__random__.uniform(-90, 90))
