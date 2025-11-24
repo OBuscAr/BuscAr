a7c55767ce26aff91df95dd08b66e225775dc654
Diego Hurtado de Mendoza <34421725+DiegoHDMGZ@users.noreply.github.com>
2025-11-24 12:37:24 -0300

    Refactor `create_line_stops` (#62)

--- backend/tests/factories/models/vehicle.py
+++ backend/tests/factories/models/vehicle.py
@@ -1,5 +1,3 @@
-import random
-
 from app.models import VehicleModel
 from polyfactory import Use
 from polyfactory.factories.sqlalchemy_factory import SQLAlchemyFactory
@@ -10,5 +8,5 @@ class VehicleFactory(SQLAlchemyFactory[VehicleModel]):
     __set_relationships__ = True
     __set_as_default_factory_for_type__ = True
 
-    latitude = Use(lambda: random.uniform(-90, 90))
-    longitude = Use(lambda: random.uniform(-90, 90))
+    latitude = Use(lambda: VehicleFactory.__random__.uniform(-90, 90))
+    longitude = Use(lambda: VehicleFactory.__random__.uniform(-90, 90))
