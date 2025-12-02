c42aad7f88c39b0c0eefd4897b4ab10abb454729
DiegoHDMGZ <dhurtadodemendoza@gmail.com>
2025-12-02 12:28:52 -0300

    update witdh

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -416,7 +416,7 @@ const DashboardPage = () => {
             </h3>
             {statistics.length > 0 ? (
               <div style={{ position: 'relative', height: '300px' }}>
-                <svg width="100%" height="100%" viewBox="0 0 600 300" preserveAspectRatio="xMidYMid meet">
+                <svg width="100%" height="100%" viewBox="0 0 650 300" preserveAspectRatio="xMidYMid meet">
                   <defs>
                     <linearGradient id="lineGradient" x1="0%" y1="0%" x2="0%" y2="100%">
                       <stop offset="0%" stopColor="#6366f1" stopOpacity="0.3" />
